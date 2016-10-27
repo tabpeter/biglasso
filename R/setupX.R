863128bd1723a6d1ae4cea81a37318efb29c2a8c
YaohuiZeng <yaohui-zeng@uiowa.edu>
2016-10-27 10:45:16 -0500

    update setupX

--- R/setupX.R
+++ R/setupX.R
@@ -6,17 +6,22 @@ setupX <- function(filename, dir = getwd(), sep = ",",
                    descriptorfile = paste0(unlist(strsplit(filename, 
                                                            split = "\\."))[1], 
                                            ".desc"), 
+                   type = 'double',
                    ...) {
+  
   # create file backing cache
   cat("Reading data from file, and creating file-backed big.matrix...\n")
   cat("This should take a while if the data is very large...\n")
   cat("Start time: ", format(Sys.time()), "\n")
-  dat <- read.big.matrix(filename = filename, sep = sep, type = 'double', 
+  dat <- read.big.matrix(filename = filename, sep = sep, type = type,
                          separated = FALSE, 
                          backingfile = backingfile, descriptorfile = descriptorfile,
                          backingpath = dir, shared = TRUE, ...)
   cat("End time: ", format(Sys.time()), "\n")
-  cat("DONE!\n")
+  cat("DONE!\n\n")
+  cat("Note: This function needs to be called only one time to create two backing\n")
+  cat("      files (.bin, .desc) in current dir. Once done, the data can be\n")
+  cat("      'loaded' using function 'attach.big.matrix'. See details in doc. \n")
   
   rm(dat)
   gc()
@@ -24,16 +29,5 @@ setupX <- function(filename, dir = getwd(), sep = ",",
   ## attach the descriptor information as the reference of the big.matrix
   X <- attach.big.matrix(descriptorfile, backingpath = dir)
   X
-}
 
-# # # ## test
-# setwd("~/GitHub/bigLasso_wide/")
-# require(bigmemory)
-# require(pryr)
-# mem_used()
-# filename <- "test_setupX.txt"
-# X <- setupX(filename, sep = "\t")
-# mem_used()
-# object_size(X)
-# dim(X)
-# X[1:10, 1:10]
+}
