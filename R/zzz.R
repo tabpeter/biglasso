43728db191df23b48a87b31f753b6d6ba87c2981
YaohuiZeng <yaohui-zeng@uiowa.edu>
2019-09-02 13:46:53 -0700

    delete zzz.R

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onUnload <- function (libpath) {
-  library.dynam.unload("biglasso", libpath)
-  try(
-    system("rm -R /tmp/boost_interprocess",
-           intern = TRUE,
-           ignore.stderr = TRUE)
-    )
-}
