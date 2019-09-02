204da3b8a7a259177ac8ca81dfcd8a48e8791f7d
YaohuiZeng <yaohui-zeng@uiowa.edu>
2019-09-01 22:18:00 -0700

    testing change order. didn't work either

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onUnload <- function (libpath) {
+  library.dynam.unload("biglasso", libpath)
   try(
     system("rm -R /tmp/boost_interprocess",
            intern = TRUE,
            ignore.stderr = TRUE)
     )
-  library.dynam.unload("biglasso", libpath)
 }
