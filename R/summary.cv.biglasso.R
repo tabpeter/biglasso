224527d74cb4f858d1e074748f1bdb909a2dda7e
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-02-07 17:09:20 -0600

    Version 1.5.0

--- R/summary.cv.biglasso.R
+++ R/summary.cv.biglasso.R
@@ -13,8 +13,7 @@
 #' non-integer printed values.
 #' @param \dots Further arguments passed to or from other methods.
 #' @return \code{summary.cv.biglasso} produces an object with S3 class
-#' \code{"summary.cv.biglasso"} which inherits class
-#' \code{"summary.cv.ncvreg"}.  The class has its own print method and contains
+#' \code{"summary.cv.biglasso"}.  The class has its own print method and contains
 #' the following list elements: \item{penalty}{The penalty used by
 #' \code{biglasso}.} \item{model}{Either \code{"linear"} or \code{"logistic"},
 #' depending on the \code{family} option in \code{biglasso}.} \item{n}{Number
@@ -39,9 +38,19 @@
 #' 
 #' 
 summary.cv.biglasso <- function(object, ...) {
-  # inherits cv.ncvreg
-  class(object) <- 'cv.ncvreg'
-  summary(object = object, ...)
+  S <- pmax(object$null.dev - object$cve, 0)
+  if (!inherits(object, 'cv.ncvsurv') && object$fit$family=="gaussian") {
+    rsq <- pmin(pmax(1 - object$cve/object$null.dev, 0), 1)
+  } else {
+    rsq <- pmin(pmax(1 - exp(object$cve-object$null.dev), 0), 1)
+  }
+  snr <- rsq/(1-rsq)
+  nvars <- predict(object$fit, type="nvars")
+  model <- switch(object$fit$family, gaussian="linear", binomial="logistic", poisson="Poisson", cox="Cox")
+  val <- list(penalty=object$fit$penalty, model=model, n=object$fit$n, p=nrow(object$fit$beta)-1, min=object$min, lambda=object$lambda, cve=object$cve, r.squared=rsq, snr=snr, nvars=nvars)
+  if (object$fit$family=="gaussian") val$sigma <- sqrt(object$cve)
+  if (object$fit$family=="binomial") val$pe <- object$pe
+  structure(val, class="summary.cv.biglasso")
 }
 
 #' @method print summary.cv.biglasso
@@ -49,7 +58,14 @@ summary.cv.biglasso <- function(object, ...) {
 #' @export
 #' 
 print.summary.cv.biglasso <- function(x, digits, ...) {
-  # inherits summary.cv.ncvreg
-  class(x) <- 'summary.cv.ncvreg'
-  print(x = x, ...)
+  digits <- if (missing(digits)) digits <- c(2, 4, 2, 2, 3) else rep(digits, length.out=5)
+  cat(x$penalty, "-penalized ", x$model, " regression with n=", x$n, ", p=", x$p, "\n", sep="")
+  cat("At minimum cross-validation error (lambda=", formatC(x$lambda[x$min], digits[2], format="f"), "):\n", sep="")
+  cat("-------------------------------------------------\n")
+  cat("  Nonzero coefficients: ", x$nvars[x$min], "\n", sep="")
+  cat("  Cross-validation error (deviance): ", formatC(min(x$cve), digits[1], format="f"), "\n", sep="")
+  cat("  R-squared: ", formatC(max(x$r.squared), digits[3], format="f"), "\n", sep="")
+  cat("  Signal-to-noise ratio: ", formatC(max(x$snr), digits[4], format="f"), "\n", sep="")
+  if (x$model == "logistic") cat("  Prediction error: ", formatC(x$pe[x$min], digits[5], format="f"), "\n", sep="")
+  if (x$model == "linear") cat("  Scale estimate (sigma): ", formatC(sqrt(x$cve[x$min]), digits[5], format="f"), "\n", sep="")
 }
